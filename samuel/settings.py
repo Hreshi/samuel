8e9c533ad07ccd8717340144d866173deba15bd2
Hreshi <59061299+Hreshi@users.noreply.github.com>
2021-02-28 15:42:52 +0530

    Update settings.py

--- samuel/settings.py
+++ samuel/settings.py
@@ -119,9 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+WHITENOISE_USE_FINDERS = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets')
 ]
-WHITENOISE_USE_FINDERS = True
